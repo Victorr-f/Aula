241ca196f2f756006f39f3cdb1519260244705c2
Victorr-f <victor.alunof@gmail.com>
2026-09-17 14:18:14 -0300

    vitor fidelix

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include __DRI__ . 'main.php';
+
+?>
---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DRI__ . '/functions/server.php';
+require_once __DRI__ . '/router/router.php';
+require_once __DRI__ . '/middleware/middeleware.php';
+require_once __DRI__ . '/dispatcher/dispatcher.php';
+require_once __DRI__ . '/controllers/usuario_controller.php';
+require_once __DRI__ . '/services/usuario_service.php';
+
+?>
